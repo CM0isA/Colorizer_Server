61e9c389e6fe8431414f5a0696a9a31fe341ee87
cm0isa <costi.moisa98@gmail.com>
2022-11-20 14:32:03 +0200

    Moded develop.py

--- Model/develop.py
+++ Model/develop.py
@@ -3,10 +3,16 @@ from keras.layers import Activation, Dense, Dropout, Flatten
 from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
-from skimage.io import imsave
+#from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
+#from skimage.io import imsave
 import numpy as np
 import os
 import random
 import tensorflow as tf
 
+# Get images
+X = []
+
+for filename in os.listdir('./TrainData'):
+    X.append(img_to_array(load_img('./TrainData/' + filename)))
+X = np.array(X, dtype=float)
