4ba280b29abcab265e666bcfa6bb1fa61cd0f789
cm0isa <costi.moisa98@gmail.com>
2022-11-21 23:41:53 +0200

    Model training

--- Model/develop.py
+++ Model/develop.py
@@ -1,10 +1,10 @@
 from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose
 from keras.layers import Activation, Dense, Dropout, Flatten
-from keras.layers.normalization import BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
 from keras.models import Sequential
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-#from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
-#from skimage.io import imsave
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
+# from skimage.io import imsave
 import numpy as np
 import os
 import random
@@ -13,6 +13,69 @@ import tensorflow as tf
 # Get images
 X = []
 
-for filename in os.listdir('./TrainData'):
-    X.append(img_to_array(load_img('./TrainData/' + filename)))
+for filename in os.listdir('./TrainData/test'):
+    image = load_img('./TrainData/test/' + filename)
+    image = image.resize((512,512))
+    image_as_array = img_to_array(image)
+    image_as_array = np.array(image_as_array, dtype=float)
+    X.append(image_as_array)
+
+
 X = np.array(X, dtype=float)
+split = int(0.95*len(X))
+Xtrain = X[:split]
+Xtrain = 1.0/255*Xtrain
+
+# Sequencial model
+model = Sequential()
+model.add(InputLayer(input_shape=(512, 512, 1)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.compile(optimizer='rmsprop', loss='mse')
+
+# Image transformer
+datagen = ImageDataGenerator(
+        shear_range=0.2,
+        zoom_range=0.2,
+        rotation_range=20,
+        horizontal_flip=True)
+
+# Generate training data
+batch_size = 10
+def image_a_b_gen(batch_size):
+    for batch in datagen.flow(Xtrain, batch_size=batch_size):
+        lab_batch = rgb2lab(batch)
+        X_batch = lab_batch[:,:,:,0]
+        Y_batch = lab_batch[:,:,:,1:] / 128
+        yield (X_batch.reshape(X_batch.shape+(1,)), Y_batch)
+
+# Train model      
+# tensorboard = tensorBoard(log_dir="output/first_run")
+model.fit(image_a_b_gen(batch_size), epochs=1, steps_per_epoch=10)
+
+# Save model
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("model.h5")
+
+
+# Test images
+Xtest = rgb2lab(1.0/255*X[split:])[:,:,:,0]
+Xtest = Xtest.reshape(Xtest.shape+(1,))
+Ytest = rgb2lab(1.0/255*X[split:])[:,:,:,1:]
+Ytest = Ytest / 128
+print(model.evaluate(Xtest, Ytest, batch_size=batch_size))
